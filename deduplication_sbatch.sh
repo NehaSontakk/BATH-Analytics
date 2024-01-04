c98d2969a0648a6f52c8afc58882bb06be069057
Neha Sontakke <nsontakke004@gmail.com>
2024-01-04 13:54:56 -0700

    Update deduplication_sbatch.sh

--- deduplication_sbatch.sh
+++ deduplication_sbatch.sh
@@ -12,7 +12,7 @@ FILE_DIR="/xdisk/twheeler/nsontakke/Prokka_db/BATH_w_prokka_db/output"
 OUTPUT_DIR="/xdisk/twheeler/nsontakke/Prokka_db/BATH_w_prokka_db/deduplication_output"
 
 # List of filenames to process
-FILES=("DNA_Bacteria_kingdom_sprot.tbl" "DNA_Archaea_kingdom_sprot.tbl" "DNA_Viruses_kingdom_sprot_ctcombined_sorted_data.tbl" "DNA_Mitochondria_kingdom_sprot_ctcombined_sorted_data.tbl" "HAMAP_bath_bin82.tbl")
+FILES=("DNA_Bacteria_IS_sprot.tbl" "DNA_Bacteria_AMR_sprot.tbl" "DNA_Bacteria_kingdom_sprot.tbl" "DNA_Archaea_kingdom_sprot.tbl" "DNA_Viruses_kingdom_sprot_ctcombined_sorted_data.tbl" "DNA_Mitochondria_kingdom_sprot_ctcombined_sorted_data.tbl" "HAMAP_bath_bin82.tbl")
 
 # Loop through each file
 for FILE in "${FILES[@]}"; do
