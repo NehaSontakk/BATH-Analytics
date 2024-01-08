86fdf7f80db2ce237b5d1a8b470a8630d3391453
Neha Sontakke <nsontakke004@gmail.com>
2024-01-08 11:42:19 -0700

    Update deduplication_sbatch.sh

--- deduplication_sbatch.sh
+++ deduplication_sbatch.sh
@@ -7,7 +7,7 @@
 
 FILE_DIR="/xdisk/twheeler/nsontakke/BATH_Prokka_DB/Output_Bathsearch"
 OUTPUT_DIR="/xdisk/twheeler/nsontakke/BATH_Prokka_DB/Output_Deduplication"
-E_VALUE="0.000001"
+E_VALUE="1e-06"
 FILES=("DNA_Bacteria_IS_sprot.tbl" "DNA_Bacteria_AMR_sprot.tbl" "DNA_Bacteria_kingdom_sprot.tbl" "DNA_Archaea_kingdom_sprot.tbl" "DNA_Viruses_kingdom_sprot_ctcombined_sorted_data.tbl" "DNA_Mitochondria_kingdom_sprot_ctcombined_sorted_data.tbl" "HAMAP_bath_bin82.tbl")
 
 # Loop through each file
